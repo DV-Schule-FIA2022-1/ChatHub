b2ec9aab8795d3dd39df60ec2c7ae163d7ffed01
fuchspatrick <fuchspatrick@dv-schulen.de>
2024-05-06 13:39:29 +0200

    Add Lombok + User Class

---
+++ src/main/java/chat/Users/User.java
@@ -0,0 +1,14 @@
+package chat.Users;
+
+import lombok.Getter;
+
+public class User
+{
+    @Getter
+    private String name;
+    @Getter
+    private String password;
+    @Getter
+    private String email;
+
+}
