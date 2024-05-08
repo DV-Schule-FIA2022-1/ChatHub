02b1fecb96848d52d8f9b8d7ef2f626703155a90
henkelmatteo <henkelmatteo@dv-schulen.de>
2024-05-08 09:32:40 +0200

    Testanfänge

---
+++ src/main/java/chat/Test/SimpleFileEditor.java
@@ -0,0 +1,18 @@
+package chat.Test;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class SimpleFileEditor extends Application
+{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/WorkTogether.fxml"));
+        Parent root = loader.load();
+//        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/EdenCodingIcon.png")));
+        primaryStage.setTitle("Work Thogether");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
