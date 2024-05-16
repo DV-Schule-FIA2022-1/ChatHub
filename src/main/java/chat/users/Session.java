470a870031964578a9a49966d2156fc19faa8d49
fuchspatrick <fuchspatrick@dv-schulen.de>
2024-05-16 14:36:09 +0200

    Preparing Session Class

--- src/main/java/chat/users/Session.java
+++ src/main/java/chat/users/Session.java
@@ -1,6 +1,37 @@
 package chat.users;
 
+import chat.message.Message;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 public class Session
 {
+    private int userID;
+    private int chatRoomID;
+    private int sessionID;
+    private long startTime;
+    private long endTime;
+    private List<Message> messages;
+    private String status;
+    private List<User> participants;
 
+    public Session(int userID, int chatRoomID, int sessionID)
+    {
+        this.userID = userID;
+        this.chatRoomID = chatRoomID;
+        this.sessionID = sessionID;
+        this.startTime = System.currentTimeMillis();
+        this.messages = new ArrayList<>();
+        this.participants = new ArrayList<>();
+        this.status = "active0";
+    }
+
+    public void endSession()
+    {
+        this.endTime = System.currentTimeMillis();
+        this.status = "inactive";
+    }
 }
