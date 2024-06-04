70d5ac90e55d9e072139cf14bedfe2e749daef11
AlowlaOmar <AlowlaOmar@dv-schulen.de>
2024-06-04 17:20:20 +0200

    added translator processor

--- src/main/java/chat/ChatBot/Main.java
+++ src/main/java/chat/ChatBot/Main.java
@@ -5,6 +5,6 @@ import com.google.gson.Gson;
 public class Main {
     public static void main(String[] args) {
         Translator test = new Translator("whats up i am omar how are u","arabic");
-        test.process("whats up i am omar how are u");
+        test.process("whats up i am omar how are you");
     }
 }
