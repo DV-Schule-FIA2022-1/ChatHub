721d93a748bac851f59c0972399a71a2fb6a914b
herberthsimon <herberthsimon@dv-schulen.de>
2024-06-07 11:03:36 +0200

    List View funktioniert

--- src/main/java/chat/Notification/NotificationController.java
+++ src/main/java/chat/Notification/NotificationController.java
@@ -1,5 +1,6 @@
 package chat.Notification;
 
+import javafx.beans.property.ListProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -7,32 +8,47 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 public class NotificationController implements Initializable
 {
     @FXML private TextField textboxMessage;
     @FXML private Button buttonSend;
+
     @FXML private ListView listboxReceived;
 
+    private ArrayList<String> messagelist;
     private NotificationMain notification;
     private String message;
 
-
+    private ListProperty<String> listProperty = new SimpleListProperty<>();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         notification = new NotificationMain();
-        System.out.println(notification.getMessagelist().toString());
+
+        messagelist = new ArrayList<>();
+        messagelist.add("hi");
+        messagelist.add("hi2");
+        System.out.println(messagelist.toString());
+
+        listProperty.set(FXCollections.observableArrayList(messagelist));
+        listboxReceived.itemsProperty().bind(listProperty);
     }
     public void sendMessage()
     {
-        listboxReceived.setItems(notification.getMessagelist());
-        //listboxReceived.setSelectionModel(notification.getMessagelist().addAll());
+        messagelist.add(textboxMessage.getText().toString());
+        listProperty.set(FXCollections.observableArrayList(messagelist));
 
-       // listboxReceived.set(notification.getMessagelist());
-       // listboxReceived.itemsProperty().bind(notification.getMessagelist());
 
+    }
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        sendMessage();
     }
     public TextField getTextboxMessage()
     {
@@ -43,4 +59,9 @@ public class NotificationController implements Initializable
     {
         return listboxReceived;
     }
+    public ArrayList<String> getMessagelist()
+    {
+        return messagelist;
+    }
+
 }
