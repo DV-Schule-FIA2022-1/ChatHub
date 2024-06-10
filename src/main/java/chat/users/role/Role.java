a518e90b4e9f76b0c7da80dbdffcd5e44fc29006
fuchspatrick <fuchspatrick@dv-schulen.de>
2024-06-10 10:46:47 +0200

    Add Role and Permission in Database

--- src/main/java/chat/users/role/Role.java
+++ src/main/java/chat/users/role/Role.java
@@ -2,23 +2,35 @@ package chat.users.role;
 
 import chat.users.User;
 import chat.users.permission.Permission;
+import lombok.Getter;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Role
 {
     private int roleID;
+    @Getter
     private String roleName;
     private Permission permissions;
+    @Getter
     private String description;
     private ArrayList<User> users;
-    private Date createDate;
-    private Date updateDate;
+    @Getter
+    private LocalDateTime createAt;
+    @Getter
+    private Date updatedAt;
+    @Getter
     private boolean isActive;
 
-    public Role()
+    public Role(String roleName, String description, LocalDateTime createAt, Date updatedAt, boolean isActive)
     {
-
+        this.roleName = roleName;
+        this.description = description;
+        this.createAt = createAt;
+        this.updatedAt = updatedAt;
+        this.isActive = isActive;
     }
 }
--- src/main/java/chat/users/role/RoleController.java
+++ src/main/java/chat/users/role/RoleController.java
@@ -1,6 +1,45 @@
 package chat.users.role;
 
+import chat.users.functions.HashFunction;
+
+import java.sql.*;
+
 public class RoleController
 {
+    private Role role;
+    private String databaseURL = "jdbc:ucanaccess://src/main/java/chat/database/TestDB.accdb";
+    private int row;
 
+    public RoleController(Role role)
+    {
+        this.role = role;
+    }
+
+    public void createRole(Role role)
+    {
+        try(Connection connection = DriverManager.getConnection(databaseURL))
+        {
+            String sql = "INSERT INTO Role (roleName, description, createAt, updatedAt, isActive) VALUES (?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, role.getRoleName());
+            preparedStatement.setString(2, role.getDescription());
+            Timestamp timestamp = Timestamp.valueOf(role.getCreateAt());
+            Date sqlDate = new Date(timestamp.getTime());
+            preparedStatement.setDate(3, sqlDate);
+            preparedStatement.setDate(4, role.getUpdatedAt());
+            preparedStatement.setBoolean(5, role.isActive());
+
+            row = preparedStatement.executeUpdate();
+
+            if(row > 0)
+            {
+                System.out.println("Data inserted sucessfully");
+            }
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
