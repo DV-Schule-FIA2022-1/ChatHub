78de6c4be7aed626e8e8cc60c855350ee4d5fe38
KolesnikowNikita <KolesnikowNikita@dv-schulen.de>
2024-06-14 12:38:47 +0200

    add spamList

---
+++ src/main/java/chat/Users/spamProtection/SaveMessageToCache.java
@@ -0,0 +1,17 @@
+package chat.Users.spamProtection;
+
+import chat.message.Message;
+
+public class SaveMessageToCache {
+
+
+    public SaveMessageToCache(Message m)
+    {
+        save(m);
+    }
+
+    private void save(Message m)
+    {
+
+    }
+}
