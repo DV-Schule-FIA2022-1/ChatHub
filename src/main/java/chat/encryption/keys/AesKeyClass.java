3fd88c39f0d4370ad1e9ef2c249b857d97b71211
KolesnikowNikita <KolesnikowNikita@dv-schulen.de>
2024-06-18 11:59:02 +0200

    add

--- src/main/java/chat/encryption/keys/AesKeyClass.java
+++ src/main/java/chat/encryption/keys/AesKeyClass.java
@@ -1,5 +1,24 @@
 package chat.encryption.keys;
 
+import javax.crypto.SecretKey;
+import java.util.Arrays;
+
 public class AesKeyClass {
+    private SecretKey key;
+
+    public AesKeyClass(SecretKey key) {
+        this.key = key;
+    }
+
+    public SecretKey getKey() {
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return "AesKeyClass{" +
+                "key=" + Arrays.toString(key.getEncoded()) +
+                '}';
+    }
 
 }
